8ec6a350b202fd4f70b93c555be306691b8090a9
Gabr7 <gabrielsisou@gmail.com>
2024-02-06 21:44:41 -0300

    adicionando tela mostrar detalhes

--- src/src/Interface/Painel.java
+++ src/src/Interface/Painel.java
@@ -4,20 +4,74 @@ import Servico.DadosServicos;
 import Servico.Servico;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Painel extends JPanel {
     DefaultListModel listaAuxiliar;
     JList<String> lista;
+    Servico servicoSelecionado;
     public Painel(DadosServicos dados){
         super(new BorderLayout());
-        listaAuxiliar = new DefaultListModel<>();
+        iniciarLista(dados);
+        adicionarSeletor(dados);
+        adicionarBotoes();
+    }
 
+    private void adicionarSeletor(DadosServicos dados) {
+        lista.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if(!e.getValueIsAdjusting()){
+                    String nomeSelecionado = lista.getSelectedValue();
+                    for(Servico servico: dados.getServicos()){
+                        if(servico.getNome().equals(nomeSelecionado)){
+                            servicoSelecionado = servico;
+                            break;
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+    private  void iniciarLista(DadosServicos dados){
+        listaAuxiliar = new DefaultListModel<>();
         for(Servico servicos: dados.getServicos()){
-            listaAuxiliar.addElement(servicos.getNome());
-        }
+            listaAuxiliar.addElement(servicos.getNome());}
         lista = new JList<>(listaAuxiliar);
         this.add(lista, BorderLayout.CENTER);
+    }
+    private void adicionarBotoes(){
+        JPanel painelBotoes = new JPanel();
+        painelBotoes.setLayout(new GridLayout(4,1));
+
+        JButton novo = new JButton("Novo");
+        painelBotoes.add(novo);
+
+        JButton visualizarDetalhes = new JButton("Visualizar Detalhes");
+        painelBotoes.add(visualizarDetalhes);
+        visualizarDetalhes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TelaDetalhes telaDetalhes = new TelaDetalhes(servicoSelecionado);
+                telaDetalhes.setVisible(true);
+            }
+        });
+
+        JButton modificar = new JButton("Modificar");
+        painelBotoes.add(modificar);
+
+        JButton salvar = new JButton("Salvar");
+        painelBotoes.add(salvar);
+
+
+
+        this.add(painelBotoes,BorderLayout.EAST);
     }
 
 }
---
+++ src/src/Interface/TelaDetalhes.java
@@ -0,0 +1,36 @@
+package Interface;
+
+import Servico.Servico;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TelaDetalhes extends JFrame {
+    private Servico servico;
+    public TelaDetalhes(Servico servico){
+        super(servico.getNome());
+        this.setSize(new Dimension(300,300));
+        this.servico = servico;
+        mostrarDetalhes();
+    }
+
+    public void mostrarDetalhes(){
+        JPanel painelDetalhes = new JPanel(new GridLayout(4,1));
+
+        JLabel nomeLabel = new JLabel("Nome: "+servico.getNome());
+        JLabel situacaoLabel = new JLabel("Situação: "+servico.getSituacao());
+        JLabel valorLabel = new JLabel("Valor do servico: "+servico.getValor());
+        JLabel recebidoLabel = new JLabel("Valor Recebido: "+servico.getRecebido());
+
+        painelDetalhes.add(nomeLabel);
+        painelDetalhes.add(situacaoLabel);
+        painelDetalhes.add(valorLabel);
+        painelDetalhes.add(recebidoLabel);
+
+        this.add(painelDetalhes);
+        this.setVisible(true);
+
+    }
+
+
+}
